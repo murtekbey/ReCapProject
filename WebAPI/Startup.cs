3c3f3fd9659e5d35650be1d5989d8b48bf75bbc1
murtekbey <murtekbey@gmail.com>
2021-08-01 15:53:09 +0300

    test

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -27,7 +27,16 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                  "CorsPolicy",
+                  builder => builder.WithOrigins("http://localhost:4200")
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials());
+            });
+
             services.AddControllers();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -66,12 +75,7 @@ namespace WebAPI
 
             app.ConfigureCustomExceptionMiddleware();
 
-            // global cors policy
-            app.UseCors(x => x
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .SetIsOriginAllowed(origin => true) // allow any origin
-                .AllowCredentials()); // allow credentials
+            app.UseCors("CorsPolicy");
 
             //app.UseHttpsRedirection();
 

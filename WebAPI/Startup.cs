4f6b50912cd45f98a09b65866f7421abe04543d5
murtekbey <murtekbey@gmail.com>
2021-08-01 16:46:04 +0300

    test

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -81,9 +81,6 @@ namespace WebAPI
 
             app.UseAuthorization();
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
